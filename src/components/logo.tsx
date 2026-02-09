050db7eb7bda73150afb6caf10e09697938461ed
Abghfor <aamr222382@gmail.com>
2026-02-09 15:53:48 +0000

    logo is too small bro

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <Link href="/">
       {/* Using a standard img tag for debugging the logo path. */}
-      <img src="/logo.png" alt="Mix Aura" width={36} height={36} className="rounded-sm" />
+      <img src="/logo.png" alt="Mix Aura" width={48} height={48} className="rounded-sm" />
     </Link>
   );
 }
